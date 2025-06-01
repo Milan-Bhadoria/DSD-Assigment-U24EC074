42e6c32a18f769bcd75c83c9b8d45f52484b6265
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-02 00:02:10 +0530

    Update FA_tb.v

--- 1_bit_FA/Behavioral/FA_tb.v
+++ 1_bit_FA/Behavioral/FA_tb.v
@@ -1,1 +1,23 @@
+`timescale 1ns/1ps
+module FA_tb;
+reg a,b,cin;
+wire s,cout;
 
+FA uut(a,b,cin,s,cout);
+
+initial begin
+$dumpfile("FA.vcd");
+$dumpvars(0,FA_tb);
+
+    a=0;b=0;cin=0;
+    #10 a=0;b=0;cin=1;
+    #10 a=0;b=1;cin=0;
+    #10 a=0;b=1;cin=1;
+    #10 a=1;b=0;cin=0;
+    #10 a=1;b=0;cin=1;
+    #10 a=1;b=1;cin=0;
+    #10 a=1;b=1;cin=1;
+    $display("done");
+    #10 $finish;
+end
+endmodule
