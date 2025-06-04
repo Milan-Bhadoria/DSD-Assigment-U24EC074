e65414e83c49c47797585e5301251401f375e546
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-04 22:36:09 +0530

    Update rca_8.v

--- RCA_8/behavioral/rca_8.v
+++ RCA_8/behavioral/rca_8.v
@@ -4,30 +4,15 @@ module rca_8(
     output reg [7:0] s,
     output reg cout
 );
-    reg c1, c2, c3, c4, c5, c6, c7;  
-  always@(*) begin
+    reg [7:0] carry; 
+    always@(*) begin
     s[0] = a[0] ^ b[0] ^ cin;
-    c1   = (a[0] & b[0]) | ((a[0] ^ b[0]) & cin);
-
-    s[1] = a[1] ^ b[1] ^ c1;
-    c2   = (a[1] & b[1]) | ((a[1] ^ b[1]) & c1);
-
-    s[2] = a[2] ^ b[2] ^ c2;
-    c3   = (a[2] & b[2]) | ((a[2] ^ b[2]) & c2);
-
-    s[3] = a[3] ^ b[3] ^ c3;
-    c4   = (a[3] & b[3]) | ((a[3] ^ b[3]) & c3);
-
-    s[4] = a[4] ^ b[4] ^ c4;
-    c5   = (a[4] & b[4]) | ((a[4] ^ b[4]) & c4);
-
-    s[5] = a[5] ^ b[5] ^ c5;
-    c6   = (a[5] & b[5]) | ((a[5] ^ b[5]) & c5);
-
-    s[6] = a[6] ^ b[6] ^ c6;
-    c7   = (a[6] & b[6]) | ((a[6] ^ b[6]) & c6);
-
-    s[7] = a[7] ^ b[7] ^ c7;
-    cout = (a[7] & b[7]) | ((a[7] ^ b[7]) & c7);
-  end
+        carry[o] = (a[0] & b[0]) | ((a[0] ^ b[0]) & cin);
+        for(int i=0;i<8;i++)begin
+            if (i<7) begin
+                carry[i] = (a[i] & b[i]) | ((a[i] ^ b[i]) & carry[i-1]);
+            end
+                s[i] = a[i] ^ b[i] ^ carry[i-1];
+            cout = (a[7] & b[7]) | ((a[7] ^ b[7]) & carry[6]);
+        end
 endmodule
