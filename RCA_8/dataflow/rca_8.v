a0f6211108df5b7a20e1220d5e7ad50565c309fe
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-04 23:09:46 +0530

    Update rca_8.v

--- RCA_8/dataflow/rca_8.v
+++ RCA_8/dataflow/rca_8.v
@@ -4,29 +4,18 @@ module rca_8(
     output [7:0] s,
     output cout
 );
-    wire c1, c2, c3, c4, c5, c6, c7;  
-
-    assign s[0] = a[0] ^ b[0] ^ cin;
-    assign c1   = (a[0] & b[0]) | ((a[0] ^ b[0]) & cin);
-
-    assign s[1] = a[1] ^ b[1] ^ c1;
-    assign c2   = (a[1] & b[1]) | ((a[1] ^ b[1]) & c1);
-
-    assign s[2] = a[2] ^ b[2] ^ c2;
-    assign c3   = (a[2] & b[2]) | ((a[2] ^ b[2]) & c2);
-
-    assign s[3] = a[3] ^ b[3] ^ c3;
-    assign c4   = (a[3] & b[3]) | ((a[3] ^ b[3]) & c3);
-
-    assign s[4] = a[4] ^ b[4] ^ c4;
-    assign c5   = (a[4] & b[4]) | ((a[4] ^ b[4]) & c4);
-
-    assign s[5] = a[5] ^ b[5] ^ c5;
-    assign c6   = (a[5] & b[5]) | ((a[5] ^ b[5]) & c5);
-
-    assign s[6] = a[6] ^ b[6] ^ c6;
-    assign c7   = (a[6] & b[6]) | ((a[6] ^ b[6]) & c6);
-
-    assign s[7] = a[7] ^ b[7] ^ c7;
-    assign cout = (a[7] & b[7]) | ((a[7] ^ b[7]) & c7);
+    wire [7:0]c;
+    genvar i;
+    generate 
+        for( i=0;i<8;i++)begin
+            if(i==0) begin
+            assign s[i] = (a[i] ^ b[i]) ^ cin ;
+            assign c[i] = (a[i] & b[i]) | ((a[i] ^ b[i]) & cin );
+        end
+            else begin
+                assign s[i] = (a[i] ^ b[i]) ^ c[i-1] ;
+                assign c[i] = (a[i] & b[i]) | ((a[i] ^ b[i]) & c[i-1] );   
+            end
+            endgenerate
+        assign cout = c[7];
 endmodule
