7d33c13486efbd0cbb2b7bda163ead3c97a89ee2
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-05 19:34:06 +0530

    Update cla_8.v

--- CLA_8/structural/cla_8.v
+++ CLA_8/structural/cla_8.v
@@ -1,29 +1,30 @@
-module fa(
-    input a,b,
-    input cin,
-    output s,cout,g,p
-); 
-    xor m1(p,a,b);
-    and m2(g,a,b);
-    and  m3(pro,p,cin);
-    or m4(cout,pro,g);
-    xor m5(s,p,cin);
-endmodule
-
-module cla(
-    input [7:0]a,b,
-    input cin,
-    output[7:0]s,
-    output cout
-);
-    wire [7:0]g,p;
-    wire [8:0] c;
-    assign c[0] = cin;
-    genvar i;
-    generate
-      for(i=0;i<8;i=i+1)begin 
-        fa fai(.a(a[i]),.b(b[i]),.cin(c[i]),.s(s[i]),.cout(c[i+1]),.g(g[i]),.p(p[i]))  ;
-      end
-    assign cout = c[7];
-    endgenerate
+module cla_4(
+   input [3:0] a,b,
+   input cin, 
+   output [3:0] s, 
+   output cout
+   );
+  wire [3:0] p,g;
+  wire [4:0] c;
+  assign p=a^b;
+  assign g=a&b;
+  assign c[0]=cin;
+  assign c[1] = g[0] | (p[0] & c[0]);
+  assign c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & c[0]);
+  assign c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] &  p[0] & c[0]);
+  assign c[4] = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c[0]);
+  
+  assign s=p^c[3:0];
+  assign cout=c[4];
+endmodule;
+module cla_8(
+  input [7:0] a,b,
+  input cin, 
+  output [7:0] s, 
+  output cout
+  );
+  wire [1:0] c;
+  cla_4 cla1(.a(a[3:0]),.b(b[3:0]),.cin(cin),.s(s[3:0]),.cout(c[0]));
+  cla_4 cla2(.a(a[7:4]),.b(b[7:4]),.cin(c[0]),.s(s[7:4]),.cout(c[1]));
+  assign cout=c[1];
 endmodule
