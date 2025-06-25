b2347e3693913964108f4899eae261ac6fef5a29
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-25 12:13:35 +0530

    Create up_counter_3bit.v

---
+++ 3bit_Counter/Asynchronous/Up_counter/up_counter_3bit.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+module flip_flop(
+    input D,
+    input clk,reset,
+    output reg Q
+    );
+    always@(posedge clk)begin
+    if(reset)begin
+    Q<=0;
+    end
+    else begin 
+    Q <= D;
+    end
+   end
+endmodule
+
+module Up_asyn(
+  input clk,reset,
+  output [2:0]Q
+); 
+  wire q0,q1,q2;
+ wire q0_bar,q1_bar,q2_bar;
+    flip_flop t1(.D(q0_bar), .clk(clk), .reset(reset), .Q(q0));
+    not n1(q0_bar,q0);
+    flip_flop t2(.D(q1_bar), .clk(q0_bar), .reset(reset), .Q(q1));
+    not n2(q1_bar,q1);
+    flip_flop t3(.D(q2_bar), .clk(q1_bar), .reset(reset), .Q(q2));
+    not n3(q2_bar,q2);
+
+   not n4(Q[0],~q0);
+   not n5(Q[1],~q1);
+   not n6(Q[2],~q2);
+endmodule
