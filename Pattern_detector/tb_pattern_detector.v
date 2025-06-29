9faaac0248b3505d7c8e945ce7345306af56d6b3
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-06-29 19:41:52 +0530

    Update tb_pattern_detector.v

--- Pattern_detector/tb_pattern_detector.v
+++ Pattern_detector/tb_pattern_detector.v
@@ -4,43 +4,15 @@ module tb_Sequence_detector();
   wire y;
 
   Sequence_detector dut(clk,x, y);
-
+  reg [31:0]bitstream = 32'b01101001110100011011110000101110;
   always #5 clk = ~clk;
 
   initial begin
     clk = 0;
-    x = 0; #10;
-x = 1; #10;
-x = 1; #10;
-x = 0; #10;
-x = 1; #10;
-x = 0; #10;
-x = 0; #10;
-x = 1; #10;
-x = 1; #10;
-x = 1; #10;
-x = 0; #10;
-x = 1; #10;
-x = 0; #10;
-x = 0; #10;
-x = 0; #10;
-x = 1; #10;
-x = 1; #10;
-x = 0; #10;
-x = 1; #10;
-x = 1; #10;
-x = 1; #10;
-x = 1; #10;
-x = 0; #10;
-x = 0; #10;
-x = 0; #10;
-x = 0; #10;
-x = 1; #10;
-x = 0; #10;
-x = 1; #10;
-x = 1; #10;
-x = 1; #10;
-x = 0; #10;
+   for(integer i=31;i>=0;i=i-1)begin
+     x = bitstream[i];
+     #10;
+   end
     $dumpfile("edge.vcd");
     $dumpvars(0, tb_Sequence_detector);
     $monitor("x=%b | y=%b | clk=%b",  x, y, clk);
