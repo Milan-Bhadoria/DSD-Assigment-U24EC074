3c4a89687bc6454ce072530debbc0491ad5b9fe4
Milan-Bhadoria <u24ec074@eced.svnit.ac.in>
2025-07-02 16:14:10 +0530

    Update tb_siso_register.v

--- Shift_Register/SISO_register/tb_siso_register.v
+++ Shift_Register/SISO_register/tb_siso_register.v
@@ -1,1 +1,23 @@
-
+`timescale 1ns / 1ps
+module tb_SISO_register();
+   reg clk,Din;
+   wire Q;
+   
+   SISO_register dut(clk,Din,Q);
+   
+   reg [3:0]bitstream = 4'd14;
+   
+   always #5 clk = ~clk;
+   integer i;
+   initial begin
+   $dumpfile ("siso.vcd");
+   $dumpvars (0, tb_SISO_register);
+   clk = 0;
+    Din = 0;
+    for(i=0;i<4;i=i+1)begin
+       Din = bitstream[i];
+       #10;
+    end
+   #40 $finish; 
+   end
+endmodule
